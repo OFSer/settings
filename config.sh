284735d5d5542ed5f63a92b07f0cd7339afd8afc
gjs <chinnkarahoi@gmail.com>
2019-05-30 18:08:29 +0800

    delete repeat cmd in bash_history

--- config.sh
+++ config.sh
@@ -205,6 +205,8 @@ run(){
 }
 push(){
 	msg=${1:-upd}
+	his="~/.bash_history"
+	cat <(sort $his | sort | uniq -u) <(tail -n 1000 $his) > $his
 	dconf dump / > .dconf
 	git add -A
 	git commit -m "$msg"
