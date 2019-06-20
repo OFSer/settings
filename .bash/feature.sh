ca2e9760cac57a2a4f6fd751ccfc6558005a8b3d
gjs <chinnkarahoi@gmail.com>
2019-06-20 13:47:26 +0800

    delete PS1.sh

--- .bash/feature.sh
+++ .bash/feature.sh
@@ -32,14 +32,38 @@ if ! shopt -oq posix; then
   fi
 fi
 
+# set variable identifying the chroot you work in (used in the prompt below)
+if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+	debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+# set a fancy prompt (non-color, unless we know we "want" color)
+case "$TERM" in
+	xterm-color|*-256color|screen) color_prompt=yes;;
+esac
+
+if [ -n "$force_color_prompt" ]; then
+	if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+		color_prompt=yes
+	else
+		color_prompt=
+	fi
+fi
+
 case "$_TERM" in
 xterm*|rxvt*)
 	PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME}:`dirs -p | head -n 1`$\007"'
 	TrapUpdate(){
-		GITREF=$(git symbolic-ref --short HEAD 2>/dev/null )
 		GITTOP=$(git rev-parse --show-toplevel 2>/dev/null)
 		TOP="$GITTOP"
-		GITSTATUS=$(git diff-files --no-ext-diff --quiet --ignore-submodules 2>/dev/null || echo '*')
+		if [ "$color_prompt" = yes ]; then
+			GITREF=$(git symbolic-ref --short HEAD 2>/dev/null )
+			GITSTATUS=$(git diff-files --no-ext-diff --quiet --ignore-submodules 2>/dev/null || echo '*')
+			[ -z "$GITREF" ] && GITPS1="" || GITPS1="[$GITREF$GITSTATUS]"
+			PS1="${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@$HOSTNAME\[\033[0m\]\[\033[01;35m\]${GITPS1}\[\033[0m\]\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\[\033[01;31m\]$\[\033[00m\] "
+		else
+			PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+		fi
 	}
 	CommandTrap(){
 		case "$BASH_COMMAND" in
